4f04942277c6a9a0059bd225efe3ec8731fa77ae
Neelesh Sai <rneeleshsai@gmail.com>
2020-11-05 15:41:23 +0530

    fixed errors

--- src/app/me/projects/project-master/project-dashboard/exceptions/exceptions-cardview/exceptions-cardview.component.ts
+++ src/app/me/projects/project-master/project-dashboard/exceptions/exceptions-cardview/exceptions-cardview.component.ts
@@ -11,6 +11,8 @@ export class ExceptionsCardviewComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  filterToggle: boolean = false;
   values:any = [
     {head:"31-10-2020 16:49:50",amperes:'38',voltage:'24',KW:'21',KVA:'29',power:'1',KWh:'2'},
     {head:"31-10-2020 16:49:50",amperes:'32',voltage:'24',KW:'21',KVA:'23',power:'1',KWh:'1'},
